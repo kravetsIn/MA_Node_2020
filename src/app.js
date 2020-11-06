b49b65ad1e6b88de9a83eb47d0edf468d2d2382e
Vladyslav Kravets <hello.kravets@gmail.com>
2020-11-06 17:53:11 +0200

    start homework 2

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
+const http = require('http');
 const products = require('../products.json');
 const { task1: filter, task2: mostExpensiveProduct, task3: formatData } = require('./task');
+const requestHandler = require('./requestHandler');
 
 function boot(data, key, value) {
   const filterRes = filter(data, key, value);
@@ -11,4 +15,12 @@ function boot(data, key, value) {
   console.log(mostExpensiveProduct);
 }
 
-boot(products, 'type', 'socks');
+// boot(products, 'type', 'socks');
+
+const server = http.createServer((req, response) => {
+  response.statusCode = 200;
+
+  response.end(process.env.PORT);
+});
+
+server.listen(process.env.PORT);
