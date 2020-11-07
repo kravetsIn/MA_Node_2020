e3f19b3da636ce24905bff6d27fe5d61528f29b8
Vladyslav Kravets <hello.kravets@gmail.com>
2020-11-07 18:56:43 +0200

    optimization code

--- src/router.js
+++ src/router.js
@@ -9,16 +9,33 @@ const {
 } = require('./controller');
 
 module.exports = (request, response) => {
-  const { method, queryParams, body: data, parseUrl } = request;
+  const {
+    method,
+    queryParams,
+    body: data,
+    parseUrl: { pathname },
+  } = request;
 
-  if (method === 'GET' && parseUrl.pathname === '/') return home(response);
-  if (method === 'GET' && parseUrl.pathname === '/filter') return filterData(response, queryParams);
-  if (method === 'GET' && parseUrl.pathname === '/max') return expensiveProduct(response);
-  if (method === 'GET' && parseUrl.pathname === '/set-default') return setDefaultData(response);
-  if (method === 'POST' && parseUrl.pathname === '/set-data')
-    return setData(data, response, queryParams);
-  if (method === 'GET' && parseUrl.pathname === '/format-data')
-    return formatDataHandler(response, queryParams);
+  switch (true) {
+    case method === 'GET' && pathname === '/':
+      return home(response);
 
-  return notFound(response);
+    case method === 'GET' && pathname === '/filter':
+      return filterData(response, queryParams);
+
+    case method === 'GET' && pathname === '/max':
+      return expensiveProduct(response);
+
+    case method === 'GET' && pathname === '/set-default':
+      return setDefaultData(response);
+
+    case method === 'POST' && pathname === '/set-data':
+      return setData(data, response, queryParams);
+
+    case method === 'GET' && pathname === '/format-data':
+      return formatDataHandler(response, queryParams);
+
+    default:
+      return notFound(response);
+  }
 };
