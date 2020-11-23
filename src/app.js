712e02a33c6d740d47d5fcf12c4d15bf4979c30f
Vladyslav Kravets <hello.kravets@gmail.com>
2020-11-23 21:51:23 +0200

    graceful shutdown

--- src/app.js
+++ src/app.js
@@ -1,3 +1,33 @@
 require('dotenv').config();
 
 const server = require('./server');
+
+function enableGracefulExit() {
+  const exitHandler = (error) => {
+    if (error) console.error(error);
+
+    console.log('Gracefully stopping...');
+    server.stop(() => {
+      process.exit();
+    });
+  };
+
+  // Catches ctrl+c event
+  process.on('SIGINT', exitHandler);
+  process.on('SIGTERM', exitHandler);
+
+  // Catches "kill pid"
+  process.on('SIGUSR1', exitHandler);
+  process.on('SIGUSR2', exitHandler);
+
+  // Catches uncaught/unhandled exceptions
+  process.on('uncaughtException', exitHandler);
+  process.on('unhandledRejection', exitHandler);
+}
+
+function boot() {
+  enableGracefulExit();
+  server.start();
+}
+
+boot();
--- src/server/index.js
+++ src/server/index.js
@@ -3,4 +3,30 @@ const requestHandler = require('./requestHandler');
 
 const server = http.createServer(requestHandler);
 
-server.listen(process.env.PORT);
+function start() {
+  const port = process.env.PORT;
+  const host = process.env.HOST;
+
+  server.listen(port, host, () => {
+    console.log(
+      `Server started: [${server.address().address}]:${server.address().port} (${
+        process.env.NODE_ENV
+      })`,
+    );
+  });
+}
+
+function stop(callback) {
+  server.close((err) => {
+    if (err) {
+      console.log(err, 'Failed to close server!');
+      callback();
+      return;
+    }
+
+    console.log('Server has beeb stopped');
+    callback();
+  });
+}
+
+module.exports = { start, stop };
